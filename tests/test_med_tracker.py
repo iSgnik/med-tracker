fff4cc8df35833cfeff56a207c1719b275a75a68
Fernando Machado de Faria <fernandomdefaria@gmail.com>
2026-05-16 13:02:16 -0300

    Update test_med_tracker.py

--- tests/test_med_tracker.py
+++ tests/test_med_tracker.py
@@ -1,10 +1,11 @@
 import pytest
 import os
 import sys
+from unittest.mock import patch
 
 # Adiciona a pasta src ao path para os testes conseguirem importar o med_tracker
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-from med_tracker import adicionar_medicamento, listar_medicamentos  # noqa: E402
+from med_tracker import adicionar_medicamento, listar_medicamentos, buscar_info_medicamento  # noqa: E402
 
 ARQUIVO_TESTE = "test_medicamentos.json"
 
@@ -41,3 +42,17 @@ def test_listar_medicamentos_banco_vazio():
     dados = listar_medicamentos(ARQUIVO_TESTE)
     assert isinstance(dados, list)
     assert len(dados) == 0
+
+
+@patch('med_tracker.requests.get')
+def test_buscar_info_medicamento_sucesso(mock_get):
+    """Teste 4: Integração com API (Simulando uma resposta de sucesso da Wikipedia)."""
+    # Configura o 'mock' (dublê) para fingir que a API retornou código 200 e um JSON com o texto
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.json.return_value = {"extract": "A Dipirona é um analgésico e antitérmico."}
+    
+    resultado = buscar_info_medicamento("Dipirona")
+    
+    # Valida se o sistema repassou a string corretamente e se chamou a URL certa
+    assert resultado == "A Dipirona é um analgésico e antitérmico."
+    mock_get.assert_called_once_with("https://pt.wikipedia.org/api/rest_v1/page/summary/Dipirona", timeout=5)
